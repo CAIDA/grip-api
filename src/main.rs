ade791c8d72d3b6aff46d7c3c6c71e63c1e64996
Mingwei Zhang <mingwei@mwzhang.com>
2018-11-01 11:46:41 -0400

    use local version of the websites

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,14 @@ use rocket::response::NamedFile;
 
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
-    NamedFile::open("gentelella/production/index.html")
+    NamedFile::open("app/index.html")
 }
 
 #[get("/<file..>")]
 fn files(file: PathBuf) -> Option<NamedFile> {
     let path_str  = file.to_str().unwrap();
-
-    // fix some paths for javascript objects
-    let file_path = {
-        if path_str.starts_with("vendors") {
-            String::from(path_str).replace("vendors", "gentelella/vendors")
-        } else if  path_str.starts_with("build"){
-            String::from(path_str).replace("build", "gentelella/build")
-        } else {
-            let mut tmp = String::from("gentelella/production/");
-            tmp.push_str(path_str);
-            tmp
-        }
-    };
+    let mut file_path = String::from("app/");
+    file_path.push_str(path_str);
 
     NamedFile::open(Path::new(&file_path)).ok()
 }
