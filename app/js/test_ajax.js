50ffd1065894de727c5bc9402cd2c38abf341a4c
Mingwei Zhang <mingwei@caida.org>
2018-11-07 11:39:57 -0800

    clean up index page, preparing for table display

--- app/js/test_ajax.js
+++ app/js/test_ajax.js
@@ -2,7 +2,7 @@ function load_ten()
 {
     $.ajax({
         type: "GET",
-        url: 'http://localhost:8000/getten',
+        url: '/getten',
         success: function(data) {
             $('#test_ajax').html(data["value"]);
         }
