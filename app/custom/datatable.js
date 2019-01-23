938bfe38eba9a0c225bd141a62e5c8fe64975098
Mingwei Zhang <mingwei@caida.org>
2019-01-23 15:03:45 -0800

    update event list page

--- app/custom/datatable.js
+++ app/custom/datatable.js
@@ -34,22 +34,140 @@ function load_events_table(event_type) {
                     "url": url,
                 },
                 "columns": [
-                    {title: "Event Type", "data": 'event_type'},
-                    {title: "Fingerprint", "data": 'fingerprint'},
-                    {title: "Prefix Events", "data": 'pfx_events_cnt'},
-                    {title: "Status", "data": 'finished_ts'},
-                    {title: "Time Stamp", "data": 'view_ts'},
+                    {title: "Poential Victim", "data": 'pfx_events'},
+                    {title: "Poential Attacker", "data": 'pfx_events'},
+                    {title: "Largest Prefix", "data": 'pfx_events'},
+                    {title: "# Prefix Events", "data": 'pfx_events'},
+                    {title: "Start Time", "data": 'view_ts'},
+                    {title: "Duration", "data": 'finished_ts'},
+                    {title: "Type", "data": 'view_ts'},
                 ],
                 "columnDefs": [
+                    {
+                        "render": function (data, type, row) {
+                            let pfxevent = data[0];
+                            switch(row["event_type"]){
+                                case "moas":
+                                    let oldcomers = new Set();
+                                    for(let i in pfxevent["origins"]){
+                                        oldcomers.add(pfxevent['origins'][i]);
+                                    }
+                                    for(let i in pfxevent["newcomer_origins"]){
+                                        oldcomers.delete(pfxevent['newcomer_origins'][i])
+                                    }
+                                    return [...oldcomers].join(" ");
+                                case "submoas":
+                                    return pfxevent["sub_origins"].join(" ");
+                                case "defcon":
+                                    return pfxevent["origins"].join(" ");
+                                case "edges":
+                                    return [pfxevent["as1"], pfxevent["as2"]].join(" ");
+                                default:
+                                    return "wrong"
+                            }
+                        },
+                        "targets": [0]
+                    },
+                    {
+                        "render": function (data, type, row) {
+                            let pfxevent = data[0];
+                            switch(row["event_type"]){
+                                case "moas":
+                                    return pfxevent["newcomer_origins"].join(" ");
+                                case "submoas":
+                                    return pfxevent["super_origins"].join(" ");
+                                case "defcon":
+                                    return "N/A";
+                                case "edges":
+                                    return [pfxevent["as1"], pfxevent["as2"]].join(" ");
+                                default:
+                                    return "wrong"
+                            }
+                        },
+                        "targets": [1]
+                    },
+                    {
+                        "render": function (data, type, row) {
+                            let largest_pfx_len = 1000;
+                            let largest_pfx = "";
+                            for(let i in data){
+                                let pfxevent = data[i];
+                                let p = "";
+                                if("prefix" in pfxevent){
+                                    p = pfxevent["prefix"];
+                                } else {
+                                    p = pfxevent["sub_pfx"];
+                                }
+                                let len = parseInt(p.split("/")[1])
+                                if(len <= largest_pfx_len){
+                                    largest_pfx = p;
+                                    largest_pfx_len = len;
+                                }
+                            }
+                            return largest_pfx;
+                        },
+                        "targets": [2]
+                    },
+                    {
+                        "render": function (data, type, row) {
+                            let num_pfx = 0;
+                            let num_addrs = 0;
+                            for(let i in data){
+                                num_pfx++;
+                                let pfxevent = data[i];
+                                let p = "";
+                                if("prefix" in pfxevent){
+                                    p = pfxevent["prefix"];
+                                } else {
+                                    p = pfxevent["sub_pfx"];
+                                }
+                                let len = parseInt(p.split("/")[1]);
+                                if(len<=32){
+                                    num_addrs += Math.pow(2, 32-len);
+                                } else {
+                                    num_addrs += Math.pow(2, 128-len);
+                                    console.log(len, num_addrs)
+                                }
+                            }
+                            if(num_addrs.toString().includes("e")){
+                                num_addrs = num_addrs.toPrecision(2)
+                            }
+                            return `${num_pfx} pfxs ${num_addrs} addresses`
+                        },
+                        "targets": [3]
+                    },
+                    {
+                        "render": function (data, type, row) {
+                            return data.split("T").join("  ")
+                        },
+                        "targets": [4]
+                    },
                     {
                         "render": function (data, type, row) {
                             if (data === null) {
                                 return "On-Going"
                             } else {
-                                return `Finished at ${data}`
+                                start_ts = Date.parse(row["view_ts"]);
+                                end_ts = Date.parse(data);
+                                return `${(end_ts-start_ts)/1000/60} min`
                             }
                         },
-                        "targets": [3]
+                        "targets": [5]
+                    },
+                    {
+                        "render": function (data, type, row) {
+                            switch( row["event_type"]){
+                                case 'moas':
+                                    return "origin hijack (moas)";
+                                case 'submoas':
+                                    return "origin hijack (submoas)";
+                                case 'edges':
+                                    return "path manipulation (new edge)";
+                                case 'defcon':
+                                    return "path manipulation (defcon)";
+                            }
+                        },
+                        "targets": [6]
                     },
                 ]
             }
