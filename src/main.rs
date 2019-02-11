f1c940acd5ea255ad70ace263e46276826074bee
Mingwei Zhang <mingwei@caida.org>
2019-02-11 14:05:33 -0800

    add parameter to specify resource directory

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,37 @@
 use rocket::fairing::AdHoc;
 use rocket::routes;
 use rocket_contrib::templates::Template;
+use structopt::StructOpt;
 
 use hijacks_dashboard::backend::api::*;
 use hijacks_dashboard::backend::api_redirects::*;
 use hijacks_dashboard::backend::data::SharedData;
 use hijacks_dashboard::backend::data::get_tag_dict;
+use std::collections::HashMap;
+use rocket::Config;
+
+#[derive(StructOpt, Debug)]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+struct Opt {
+    #[structopt(short = "d")]
+    directory: Option<String>,
+}
 
 fn main() {
-    rocket::ignite()
+    let opt = Opt::from_args();
+    let resource_dir: String;
+    match opt.directory {
+        Some(d) => resource_dir = d,
+        None => resource_dir = "./".to_owned()
+    };
+
+    let mut config = Config::active().unwrap();
+    // set template directory
+    let mut extra_config = HashMap::new();
+    extra_config.insert("template_dir".to_owned(), format!("{}/templates",&resource_dir).into());
+    config.set_extras(extra_config);
+
+    rocket::custom(config.clone())
         .mount(
             "/",
             routes![
@@ -28,13 +51,13 @@ fn main() {
             ],
         )
         .attach(AdHoc::on_attach("get elastic search url", |rocket| {
-            let es_url = rocket
-                .config()
-                .get_str("elastic_url")
-                .unwrap_or("http://clayface.caida.org:9200")
-                .to_string();
+            // set ElasticSearch URL
+            let es_url = rocket.config().get_str("elastic_url")
+                .unwrap_or("http://clayface.caida.org:9200") .to_string();
+
+            // pass in tags
             let tag_dict = get_tag_dict();
-            Ok(rocket.manage(SharedData { es_url, tag_dict}))
+            Ok(rocket.manage(SharedData { es_url, tag_dict, resource_dir}))
         }))
         .attach(Template::fairing())
         .launch();
