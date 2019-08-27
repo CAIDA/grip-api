48630a898ae62445a4b4d2e0889af116ed570d2d
Mingwei Zhang <mingwei@caida.org>
2019-08-26 17:07:54 -0700

    update event list api to incorporate suspicion value

--- src/backend/api.rs
+++ src/backend/api.rs
@@ -10,11 +10,10 @@ use rocket_contrib::templates::Template;
 use serde_json::json;
 use serde_json::Value;
 
+use crate::backend::data::*;
 use crate::backend::elastic::ElasticSearchBackend;
 use crate::backend::utils::*;
-use crate::backend::data::*;
 use rocket::response::Redirect;
-
 
 /// load static content
 #[get("/app/<file..>")]
@@ -38,62 +37,92 @@ pub fn page_index() -> Redirect {
 
 /// load events list page
 #[get("/events/<_event_type>?<debug>")]
-pub fn page_event_list(_event_type: &RawStr, debug: Option<bool>, _data: State<SharedData>) -> Template {
+pub fn page_event_list(
+    _event_type: &RawStr,
+    debug: Option<bool>,
+    _data: State<SharedData>,
+) -> Template {
     let mut context = HashMap::<String, Value>::new();
     let show_debug = match debug {
         Some(b) => b,
-        None => false
+        None => false,
     };
-    context.insert("context".to_owned(), json!({
-        "onload_function":"load_events_table()" ,
-        "debug": show_debug,
-    }));
+    context.insert(
+        "context".to_owned(),
+        json!({
+            "onload_function":"load_events_table()" ,
+            "debug": show_debug,
+        }),
+    );
 
     Template::render("event_list", context)
 }
 
 /// load events list page
 #[get("/events_benign/<_event_type>?<debug>")]
-pub fn page_benign_event_list(_event_type: &RawStr, debug: Option<bool>, _data: State<SharedData>) -> Template {
+pub fn page_benign_event_list(
+    _event_type: &RawStr,
+    debug: Option<bool>,
+    _data: State<SharedData>,
+) -> Template {
     let mut context = HashMap::<String, Value>::new();
     let show_debug = match debug {
         Some(b) => b,
-        None => false
+        None => false,
     };
-    context.insert("context".to_owned(), json!({
-        "onload_function":"load_events_table(true)" ,
-        "debug": show_debug,
-    }));
+    context.insert(
+        "context".to_owned(),
+        json!({
+            "onload_function":"load_events_table(true)" ,
+            "debug": show_debug,
+        }),
+    );
     Template::render("event_list", context)
 }
 
 /// load event details page
 #[get("/events/<_event_type>/<_id>?<debug>")]
-pub fn page_event_details(_event_type: &RawStr, debug: Option<bool>, _id: &RawStr, _data: State<SharedData>) -> Template {
+pub fn page_event_details(
+    _event_type: &RawStr,
+    debug: Option<bool>,
+    _id: &RawStr,
+    _data: State<SharedData>,
+) -> Template {
     let mut context = HashMap::<String, Value>::new();
     let show_debug = match debug {
         Some(b) => b,
-        None => false
+        None => false,
     };
-    context.insert("context".to_owned(), json!({
-        "onload_function":"load_event_details()",
-        "debug": show_debug,
-    }));
+    context.insert(
+        "context".to_owned(),
+        json!({
+            "onload_function":"load_event_details()",
+            "debug": show_debug,
+        }),
+    );
     Template::render("event_detail", context)
 }
 
 /// load pfx_event details page
 #[get("/events/<_event_type>/<_id>/<pfx_finger_print>")]
-pub fn page_traceroutes_page(_event_type: &RawStr, _id: &RawStr, pfx_finger_print: &RawStr, _data: State<SharedData>) -> Template {
-    dbg!(pfx_finger_print.to_string().replace("-","\\/"));
+pub fn page_traceroutes_page(
+    _event_type: &RawStr,
+    _id: &RawStr,
+    pfx_finger_print: &RawStr,
+    _data: State<SharedData>,
+) -> Template {
+    dbg!(pfx_finger_print.to_string().replace("-", "\\/"));
     let address_str = pfx_finger_print.to_string();
     let v: Vec<&str> = address_str.split('-').collect();
     let mut context = HashMap::<String, Value>::new();
-    context.insert("context".to_owned(), json!({
-        "onload_function":"load_pfx_event()",
-        "address": v[0],
-        "mask": v[1],
-    }));
+    context.insert(
+        "context".to_owned(),
+        json!({
+            "onload_function":"load_pfx_event()",
+            "address": v[0],
+            "mask": v[1],
+        }),
+    );
     Template::render("event_traceroutes", context)
 }
 
@@ -101,9 +130,12 @@ pub fn page_traceroutes_page(_event_type: &RawStr, _id: &RawStr, pfx_finger_prin
 #[get("/blacklist")]
 pub fn page_blacklist(_data: State<SharedData>) -> Template {
     let mut context = HashMap::<String, Value>::new();
-    context.insert("context".to_owned(), json!({
-        "onload_function":"load_blacklist()",
-    }));
+    context.insert(
+        "context".to_owned(),
+        json!({
+            "onload_function":"load_blacklist()",
+        }),
+    );
     Template::render("blacklist", context)
 }
 
@@ -111,9 +143,12 @@ pub fn page_blacklist(_data: State<SharedData>) -> Template {
 #[get("/tags")]
 pub fn page_tags(_data: State<SharedData>) -> Template {
     let mut context = HashMap::<String, Value>::new();
-    context.insert("context".to_owned(), json!({
-        "onload_function":"load_tags()",
-    }));
+    context.insert(
+        "context".to_owned(),
+        json!({
+            "onload_function":"load_tags()",
+        }),
+    );
     Template::render("tags", context)
 }
 
@@ -123,18 +158,20 @@ JSON QUERY APIS
 
 #[get("/json/tags")]
 pub fn json_get_tags() -> Json<Value> {
-    let tags: Value = reqwest::get(format!("http://10.250.203.3:5000/tags/moas").as_str()).unwrap().json().unwrap();
-    Json(
-        json!(tags)
-    )
+    let tags: Value = reqwest::get(format!("http://10.250.203.3:5000/tags/moas").as_str())
+        .unwrap()
+        .json()
+        .unwrap();
+    Json(json!(tags))
 }
 
 #[get("/json/blacklist")]
 pub fn json_get_blacklist() -> Json<Value> {
-    let blacklist: Value = reqwest::get(format!("http://10.250.203.3:5000/blacklist").as_str()).unwrap().json().unwrap();
-    Json(
-        json!(blacklist)
-    )
+    let blacklist: Value = reqwest::get(format!("http://10.250.203.3:5000/blacklist").as_str())
+        .unwrap()
+        .json()
+        .unwrap();
+    Json(json!(blacklist))
 }
 
 #[get("/json/event/id/<id>")]
@@ -151,17 +188,17 @@ pub fn json_event_by_id(id: &RawStr, base_url: State<SharedData>) -> Json<Value>
         Ok(event) => {
             let e = process_raw_event(&event.results[0], true);
             Json(json!(e))
-        },
-        Err(_e) => Json(json!(
-        {
-            "error": format!("Cannot find event {}",id)
         }
-        )),
+        Err(_e) => Json(json!({ "error": format!("Cannot find event {}", id) })),
     }
 }
 
 #[get("/json/pfx_event/id/<id>/<fingerprint>")]
-pub fn json_pfx_event_by_id(id: &RawStr, fingerprint: &RawStr, base_url: State<SharedData>) -> Json<Value> {
+pub fn json_pfx_event_by_id(
+    id: &RawStr,
+    fingerprint: &RawStr,
+    base_url: State<SharedData>,
+) -> Json<Value> {
     let backend_res = ElasticSearchBackend::new(&base_url.es_url);
 
     let backend = match backend_res {
@@ -172,16 +209,12 @@ pub fn json_pfx_event_by_id(id: &RawStr, fingerprint: &RawStr, base_url: State<S
     match backend.get_event_by_id(id) {
         Ok(event) => {
             match filter_pfx_events_by_fingerprint(fingerprint.as_str(), &event.results[0]) {
-                Some(event) => {
-                    Json(json!(event.to_owned()))
-                }
-                None => {
-                    Json(json!(
-                        {
-                            "error": "Cannot find prefix event"
-                        }
-                    ))
-                }
+                Some(event) => Json(json!(event.to_owned())),
+                None => Json(json!(
+                    {
+                        "error": "Cannot find prefix event"
+                    }
+                )),
             }
         }
         Err(_e) => Json(json!(
@@ -192,15 +225,33 @@ pub fn json_pfx_event_by_id(id: &RawStr, fingerprint: &RawStr, base_url: State<S
     }
 }
 
-#[get("/json/events/<event_type>?<ts_start>&<ts_end>&<draw>&<start>&<length>&<asn>&<prefix>&<benign>&<tags>")]
-pub fn json_list_events(event_type: &RawStr, ts_start: Option<String>, ts_end: Option<String>,
-                        draw: Option<usize>, start: Option<usize>, length: Option<usize>,
-                        asn: Option<usize>, prefix: Option<String>, benign: Option<bool>,
-                        tags: Option<String>,
-                        base_url: State<SharedData>) -> Json<Value> {
+#[get("/json/events/<event_type>?<ts_start>&<ts_end>&<draw>&<start>&<length>&<asn>&<prefix>&<tags>&<min_susp>&<max_susp>")]
+pub fn json_list_events(
+    event_type: &RawStr,
+    ts_start: Option<String>,
+    ts_end: Option<String>,
+    draw: Option<usize>,
+    start: Option<usize>,
+    length: Option<usize>,
+    asn: Option<usize>,
+    prefix: Option<String>,
+    tags: Option<String>,
+    min_susp: Option<usize>,
+    max_susp: Option<usize>,
+    base_url: State<SharedData>,
+) -> Json<Value> {
     let backend = ElasticSearchBackend::new(&base_url.es_url).unwrap();
-    let query_result = backend.list_events(event_type, &start, &length, &asn, &prefix, &ts_start, &ts_end, &benign, &tags).unwrap();
-    let res_data: Vec<Value> = query_result.results.iter().map(|v| process_raw_event(v, false)).collect();
+    let query_result = backend
+        .list_events(
+            event_type, &start, &length, &asn, &prefix, &ts_start, &ts_end,  &tags,
+            &min_susp, &max_susp,
+        )
+        .unwrap();
+    let res_data: Vec<Value> = query_result
+        .results
+        .iter()
+        .map(|v| process_raw_event(v, false))
+        .collect();
     let object = json!(
         {
             "data": res_data,
@@ -219,15 +270,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_data_filtering(){
+    fn test_data_filtering() {
         let backend = ElasticSearchBackend::new(&"http://clayface.caida.org:9200").unwrap();
-        let query_result = backend.list_events(&"moas", &Some(0), &Some(1), &None, &None, &None, &None, &None, &None).unwrap();
-        let res_data: Vec<Value> = query_result.results.iter().map(|v| process_raw_event(v, false)).collect();
+        let query_result = backend
+            .list_events(
+                &"moas",
+                &Some(0),
+                &Some(1),
+                &None,
+                &None,
+                &None,
+                &None,
+                &None,
+                &None,
+                &None,
+            )
+            .unwrap();
+        let res_data: Vec<Value> = query_result
+            .results
+            .iter()
+            .map(|v| process_raw_event(v, false))
+            .collect();
         println!("{}", serde_json::to_string_pretty(&res_data).unwrap());
-        println!("{}/{}",
-                 serde_json::to_string_pretty(&res_data).unwrap().len(),
-                 serde_json::to_string_pretty(&query_result.results).unwrap().len(),
+        println!(
+            "{}/{}",
+            serde_json::to_string_pretty(&res_data).unwrap().len(),
+            serde_json::to_string_pretty(&query_result.results)
+                .unwrap()
+                .len(),
         );
     }
-
 }
--- src/backend/elastic.rs
+++ src/backend/elastic.rs
@@ -1,7 +1,7 @@
-use std::error::Error;
 use chrono::prelude::DateTime;
-use chrono::{Utc};
-use std::time::{UNIX_EPOCH, Duration};
+use chrono::Utc;
+use std::error::Error;
+use std::time::{Duration, UNIX_EPOCH};
 
 use elastic::prelude::*;
 use serde_json::json;
@@ -36,84 +36,91 @@ impl ElasticSearchBackend {
         let d = UNIX_EPOCH + Duration::from_secs(view_ts);
         let datetime = DateTime::<Utc>::from(d);
 
-        let query = format!("http://clayface.caida.org:9200/hijacks-{}-{}-{:02}-{:02}/event_result/{}",
-                    event_type,
-                    datetime.year(),
-                    datetime.month(),
-                    datetime.day(),
-                    id);
+        let query = format!(
+            "http://clayface.caida.org:9200/hijacks-{}-{}-{:02}-{:02}/event_result/{}",
+            event_type,
+            datetime.year(),
+            datetime.month(),
+            datetime.day(),
+            id
+        );
 
-        let doc: Value = reqwest::get(query.as_str())
-            .unwrap().json().unwrap();
+        let doc: Value = reqwest::get(query.as_str()).unwrap().json().unwrap();
         if doc["found"] == true {
-            return Ok(SearchResult { results: vec!(doc["_source"].clone()), total: 1 });
+            return Ok(SearchResult {
+                results: vec![doc["_source"].clone()],
+                total: 1,
+            });
         } else {
             Err(Box::new(MyError("Oops".into())))
         }
     }
 
-    pub fn list_events(&self, event_type: &str, start: &Option<usize>, max: &Option<usize>,
-                       asn: &Option<usize>, prefix: &Option<String>,
-                       ts_start: &Option<String>, ts_end: &Option<String>, benign: &Option<bool>,
-                       tags: &Option<String>)
-                       -> Result<SearchResult, Box<dyn Error>> {
+    pub fn list_events(
+        &self,
+        event_type: &str,
+        start: &Option<usize>,
+        max: &Option<usize>,
+        asn: &Option<usize>,
+        prefix: &Option<String>,
+        ts_start: &Option<String>,
+        ts_end: &Option<String>,
+        tags: &Option<String>,
+        min_susp: &Option<usize>,
+        max_susp: &Option<usize>,
+    ) -> Result<SearchResult, Box<dyn Error>> {
         let mut etype = event_type.to_owned();
-
-        // default we want suspicious events, unless specified otherwise
-        let mut want_suspicious = true;
-        let mut want_misconf = false;
 
         if etype == "all" {
             // if event type is all or misconf (misconfiguration), show all events that matches
             etype = "*".to_owned();
         }
-        if etype == "misconf" {
-            // if event type is all or misconf (misconfiguration), show all events that matches
-            etype = "*".to_owned();
-            want_misconf = true;
-            want_suspicious = true;
+
+        let mut query_from = 0;
+        if let Some(s) = start {
+            query_from = s.to_owned() as i32;
         }
 
-        if let Some(want_benign) = benign {
-            if *want_benign {
-                want_suspicious = false;
-                want_misconf = false
-            }
-        };
-
         let mut range_filter = json!({"view_ts":{}});
-        match ts_start {
-            Some(start_str) => range_filter["view_ts"]["gte"] = json!(start_str),
-            _ => {}
-        };
-        match ts_end {
-            Some(end_str) => range_filter["view_ts"]["lte"] = json!(end_str),
-            _ => {}
-        };
+        if let Some(start_str) = ts_start {
+            range_filter["view_ts"]["gte"] = json!(start_str);
+        }
+        if let Some(end_str) = ts_end {
+            range_filter["view_ts"]["lte"] = json!(end_str);
+        }
 
         let max_entries = match max {
             Some(n) => n.to_owned() as i32,
-            None => 100 as i32
+            None => 100 as i32,
         };
 
         // match must terms
-        let mut must_terms = vec!();
-        let mut must_not_terms = vec!();
+        let mut must_terms = vec![];
+        let mut must_not_terms = vec![];
         must_not_terms.push(json!({ "match": { "position.keyword": "FINISHED" } }));
 
-        if want_misconf {
-            must_terms.push(json!({ "term": { "inference.misconfiguration" : true }}));
-        } else if want_suspicious {
-            must_terms.push(json!({ "term": { "inference.suspicious" : true }}));
-        } else {
-            must_terms.push(json!({ "term": { "inference.misconfiguration" : false }}));
-            must_terms.push(json!({ "term": { "inference.suspicious" : false }}));
+        // if want_misconf {
+        //     must_terms.push(json!({ "term": { "inference.misconfiguration" : true }}));
+        // } else if want_suspicious {
+        //     must_terms.push(json!({ "term": { "inference.suspicious" : true }}));
+        // } else {
+        //     must_terms.push(json!({ "term": { "inference.misconfiguration" : false }}));
+        //     must_terms.push(json!({ "term": { "inference.suspicious" : false }}));
+        // }
+
+        let mut suspicion_filter = json!({"inference.suspicion.suspicion_level": {}});
+        if let Some(max) = max_susp {
+            suspicion_filter["inference.suspicion.suspicion_level"]["lte"] = json!(max.to_owned() as i32);
         }
+        if let Some(min) = min_susp {
+            suspicion_filter["inference.suspicion.suspicion_level"]["gte"] = json!(min.to_owned() as i32);
+        }
+        must_terms.push(json!({"range": suspicion_filter} ));
 
         match prefix {
             Some(p) => {
                 // https://stackoverflow.com/questions/40573981/multiple-should-queries-with-must-query
-                let mut pfx_must = vec!();
+                let mut pfx_must = vec![];
                 pfx_must.push(json!({ "prefix": { "pfx_events.sub_pfx.keyword" : p }}));
                 pfx_must.push(json!({ "prefix": { "pfx_events.super_pfx.keyword" : p }}));
                 pfx_must.push(json!({ "prefix": { "pfx_events.prefix.keyword" : p }}));
@@ -124,7 +131,7 @@ impl ElasticSearchBackend {
         match asn {
             Some(value) => {
                 // https://stackoverflow.com/questions/40573981/multiple-should-queries-with-must-query
-                let mut asn_must = vec!();
+                let mut asn_must = vec![];
                 asn_must.push(json!({ "match": { "pfx_events.origins" : value }}));
                 asn_must.push(json!({ "match": { "pfx_events.as1.keyword" : value }}));
                 asn_must.push(json!({ "match": { "pfx_events.as2.keyword" : value }}));
@@ -135,7 +142,7 @@ impl ElasticSearchBackend {
         match tags {
             Some(tags_string) => {
                 let tags_lst: Vec<&str> = tags_string.split(",").collect::<Vec<&str>>();
-                for t in tags_lst{
+                for t in tags_lst {
                     if t.starts_with("!") {
                         // negative match
                         let new_t = t.trim_start_matches('!');
@@ -149,21 +156,21 @@ impl ElasticSearchBackend {
         }
 
         let query: serde_json::Value = json!({
-                "from":start, "size":max_entries,
-                "query": {
-                    "bool": {
-                        "must": must_terms,
-                        "must_not": must_not_terms,
-                        "filter": {
-                            "range": range_filter
-                        }
+            "from":query_from, "size":max_entries,
+            "query": {
+                "bool": {
+                    "must": must_terms,
+                    "must_not": must_not_terms,
+                    "filter": {
+                        "range": range_filter
                     }
-                },
-                "sort": { "view_ts": { "order": "desc" }}
-            });
+                }
+            },
+            "sort": { "view_ts": { "order": "desc" }}
+        });
 
         // DEBUG line below
-        // println!("{}", serde_json::to_string_pretty(&query).unwrap());
+        println!("{}", serde_json::to_string_pretty(&query).unwrap());
 
         let res = self
             .es_client
@@ -180,6 +187,9 @@ impl ElasticSearchBackend {
             res_vec.push(doc);
         }
 
-        Ok(SearchResult { results: res_vec, total: res.total() })
+        Ok(SearchResult {
+            results: res_vec,
+            total: res.total(),
+        })
     }
 }
